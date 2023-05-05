a84751ae87ddc5a0622f27f93eaebd06e8e5ee9f
Krishna <libraryismyparadise@gmail.com>
2023-05-06 04:13:47 +0545

    Fix header__socials-icon

--- src/features/Contact/Contact.jsx
+++ src/features/Contact/Contact.jsx
@@ -1,57 +1,101 @@
-import React, {useRef} from 'react'
-import emailjs from 'emailjs-com'
-import {MdOutlineMailOutline} from 'react-icons/md'
-import {RiMessengerLine} from 'react-icons/ri'
-import {BsWhatsapp} from 'react-icons/bs'
-import './contact.css'
+import React, { useState, useRef } from 'react';
+import emailjs from 'emailjs-com';
+import { MdOutlineMailOutline } from 'react-icons/md';
+import { RiMessengerLine } from 'react-icons/ri';
+import { BsWhatsapp, BsFillChatHeartFill } from 'react-icons/bs';
+import './contact.css';
 
 const Contact = () => {
   const form = useRef();
+  const [showSuccess, setShowSuccess] = useState(false);
 
   const sendEmail = (e) => {
     e.preventDefault();
-
-    emailjs.sendForm('service_uz945tq', 'template_iq7v88q', form.current, 'PU1Fo1Ro9TD7ws9sE')
-    e.target.reset()
-      .then((result) => {
+    emailjs
+      .sendForm(
+        'service_uz945tq',
+        'template_iq7v88q',
+        form.current,
+        'PU1Fo1Ro9TD7ws9sE'
+      )
+      .then(
+        (result) => {
           console.log(result.text);
-      }, (error) => {
+          setShowSuccess(true);
+          setTimeout(() => setShowSuccess(false), 6000);
+        },
+        (error) => {
           console.log(error.text);
-      });
+        }
+      );
+    e.target.reset();
   };
 
   return (
     <section id='contact'>
-      <h5>If you have an application you are interested in developing, a feature that you need built or a project that needs coding. I’d love to help with it</h5>
+      <h5>
+        If you have an application you are interested in developing, a feature
+        that you need built or a project that needs coding. I’d love to help
+        with it
+      </h5>
       <h2>Contact Me</h2>
       <div className='container contact__container'>
-      <div className="contact__options">
-        <article className="contact__option">
-        <MdOutlineMailOutline className='contact__option-icon'/>
-          <h4>Email</h4>
-          <a href="mailto:libraryismyparadise@gmail.com" target='_blank' rel="noreferrer">Send me a Email</a>
-        </article>
-        <article className="contact__option">
-        <RiMessengerLine className='contact__option-icon'/>
-          <h4>Messenger</h4>
-          <a href="https://m.me/wolverinekrishna" target='_blank' rel="noreferrer">Send me a DM</a>
-        </article>
-        <article className="contact__option">
-        <BsWhatsapp className='contact__option-icon'/>
-          <h4>Whatsapp</h4>
-          <a href=" https://wa.me/9779816398914" target='_blank' rel="noreferrer">Send me a Whatsapp</a>
-        </article>
+        <div className='contact__options'>
+          <article className='contact__option'>
+            <MdOutlineMailOutline className='contact__option-icon' />
+            <h4>Email</h4>
+            <a
+              href='mailto:libraryismyparadise@gmail.com'
+              target='_blank'
+              rel='noreferrer'
+            >
+              Send me a Email
+            </a>
+          </article>
+          <article className='contact__option'>
+            <RiMessengerLine className='contact__option-icon' />
+            <h4>Messenger</h4>
+            <a
+              href='https://m.me/wolverinekrishna'
+              target='_blank'
+              rel='noreferrer'
+            >
+              Send me a DM
+            </a>
+          </article>
+          <article className='contact__option'>
+            <BsWhatsapp className='contact__option-icon' />
+            <h4>Whatsapp</h4>
+            <a
+              href=' https://wa.me/9779816398914'
+              target='_blank'
+              rel='noreferrer'
+            >
+              Send me a Whatsapp
+            </a>
+          </article>
+        </div>
+        <form ref={form} onSubmit={sendEmail}>
+          <input type='text' name='name' placeholder='Your Full Name' required />
+          <input type='email' name='email' placeholder='Your Email Address' required />
+          <textarea name='message' placeholder='Your Messege' rows='7' required></textarea>
+          <button type='submit' className='btn btn-primary'>
+            Get in Touch
+          </button>
+          {showSuccess && (
+            <div className='success-message'>
+              <div>
+              <BsFillChatHeartFill className='smily-icon'/>
+              <BsFillChatHeartFill className='smily-icon'/>
+              <BsFillChatHeartFill className='smily-icon'/>
+              </div>
+              <h5>Thank you for contacting. I will get back to you soon.</h5>
+              </div>
+          )}
+        </form>
       </div>
-      <form ref={form} onSubmit={sendEmail}>
-        <input type='text' name='name' placeholder="Your Full Name"  required />
-        <input type='email' name = 'email' placeholder="Your Email Address" required />
-        <textarea name='message' placeholder='Your Messege' rows="7" required></textarea>
-        <button type="submit" className='btn btn-primary'>Get in Touch</button>
-      </form>
-       </div>
-
     </section>
-  )
-}
+  );
+};
 
 export default Contact
--- src/features/Header/Socials.jsx
+++ src/features/Header/Socials.jsx
@@ -6,12 +6,12 @@ import {FaAngellist, FaHackerrank} from 'react-icons/fa'
 function Socials() {
   return (
     <div className='header__socials'>
-        <a href='https://www.linkedin.com/in/krishnabot/' target='_blank' rel="noreferrer"><BsLinkedin /></a>
-        <a href='https://github.com/Krishnabot' target='_blank' rel="noreferrer"><BsGithub /></a>
-        <a href='https://twitter.com/last_matrix' target='_blank' rel="noreferrer"><BsTwitter /></a>
-        <a href='https://medium.com/@Krishna_' target='_blank'  rel="noreferrer">< BsMedium /></a>
-        <a href='https://wellfound.com/u/krishnabot' target='_blank' rel="noreferrer"><FaAngellist /></a>
-        <a href='https://www.hackerrank.com/KrishnaBot' target='_blank' rel="noreferrer">< FaHackerrank /></a>
+        <a href='https://www.linkedin.com/in/krishnabot/' target='_blank' rel="noreferrer"><BsLinkedin className='header__socials-icon' /></a>
+        <a href='https://github.com/Krishnabot' target='_blank' rel="noreferrer"><BsGithub className='header__socials-icon'/></a>
+        <a href='https://twitter.com/last_matrix' target='_blank' rel="noreferrer"><BsTwitter className='header__socials-icon'/></a>
+        <a href='https://medium.com/@Krishna_' target='_blank'  rel="noreferrer">< BsMedium className='header__socials-icon'/></a>
+        <a href='https://wellfound.com/u/krishnabot' target='_blank' rel="noreferrer"><FaAngellist className='header__socials-icon'/></a>
+        <a href='https://www.hackerrank.com/KrishnaBot' target='_blank' rel="noreferrer">< FaHackerrank className='header__socials-icon'/></a>
 
     </div>
   )
