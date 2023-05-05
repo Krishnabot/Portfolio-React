c185329dfc31ec713cd271012c3153e4e365a51a
Krishna <libraryismyparadise@gmail.com>
2023-05-06 01:59:10 +0545

    Add Active class to Current Page

--- src/features/Nav/Nav.jsx
+++ src/features/Nav/Nav.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { AiOutlineHome, AiOutlineUser, AiOutlineUnorderedList } from 'react-icons/ai';
 import { BiBook, BiMessageSquareDetail } from 'react-icons/bi';
 import { RiServiceLine } from 'react-icons/ri';
@@ -15,13 +15,61 @@ const NAV_ITEMS = [
 
 const Nav = () => {
   const [activeNav, setActiveNav] = useState('#');
+  const navRef = useRef(null);
 
   const handleNavClick = (id) => {
     setActiveNav(id);
   };
 
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            setActiveNav(`#${entry.target.id}`);
+          }
+        });
+      },
+      { rootMargin: '-51% -10% -51% -10%' }
+
+    );
+
+    document.querySelectorAll('section').forEach((section) => {
+      observer.observe(section);
+    });
+
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPos = window.pageYOffset;
+      const sections = document.querySelectorAll('section');
+      const navHeight = navRef.current.offsetHeight;
+      const offset = navHeight / 2;
+
+      sections.forEach((section) => {
+        const sectionTop = section.offsetTop - offset;
+        const sectionBottom = sectionTop + section.offsetHeight;
+
+        if (currentScrollPos >= sectionTop && currentScrollPos < sectionBottom) {
+          setActiveNav(`#${section.id}`);
+        }
+      });
+
+      if (currentScrollPos < sections[0].offsetTop) {
+        setActiveNav('#');
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
-    <nav>
+    <nav ref={navRef}>
       {NAV_ITEMS.map((item) => (
         <a
           key={item.id}
