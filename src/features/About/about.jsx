5abf2a8f5efbadb7885eb1897f9734a0da3effd1
Krishna <libraryismyparadise@gmail.com>
2023-05-05 16:07:46 +0545

    Style About Section

--- src/features/About/about.jsx
+++ src/features/About/about.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './about.css'
-import Self_Image from '../../assets/Image/Self_Image.png'
+import Self_Image from '../../assets/Image/self_Image.jpg'
 import {FaAward} from 'react-icons/fa'
 import {FiUsers} from 'react-icons/fi'
 import {VscFolderLibrary} from 'react-icons/vsc'
