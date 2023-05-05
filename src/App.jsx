d7385e830568b42cafefd8794def8b17d3132dc8
Krishna <libraryismyparadise@gmail.com>
2023-05-05 11:12:03 +0545

    Add basic structure of the Portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
-import './App.css';
+import React from 'react';
+import Header from './features/Header/Header'
+import Nav from './features/Nav/Nav'
+import About from './features/About/About'
+import Experience from './features/Experience/Experience'
+import Services from './features/Services/Services'
+import Portfolio from './features/Portfolio/Portfolio'
+import Endorsement from './features/Endorsement/Endorsement'
+import Contact from './features/Contact/Contact'
+import Footer from './features/Footer/Footer'
+
 
 function App() {
   return (
-<h1>Hello</h1>
+    <>
+      < Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Services />
+      <Portfolio />
+      <Endorsement />
+      <Contact />
+      <Footer />
+</>
   );
 }
 
---
+++ src/features/Endorsement/Endorsement.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './endorsement.css'
+
+function Endorsement() {
+  return (
+    <div>Endorsement</div>
+  )
+}
+
+export default Endorsement
